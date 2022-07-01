e100dbd736b82d869f7f4c8db9d3919796fa663b
joas77 <solojoas@gmail.com>
2022-06-30 23:24:39 -0500

    Minor changes to allow user to continue using mouse

--- mouse_mov.py
+++ mouse_mov.py
@@ -1,12 +1,14 @@
 import random
 import pyautogui
+import time
 
 if __name__=="__main__":
     print("moving mouse randomly to avoid station going to sleep mode")
     print("press ctrl-C to cancel...")
 
     while True:
-        x = random.randint(-100, 100)
-        y = random.randint(-100, 100)
+        x = random.randint(-10, 10)
+        y = random.randint(-10, 10)
 
         pyautogui.moveRel(x,y, duration=0.5)
+        time.sleep(5)
